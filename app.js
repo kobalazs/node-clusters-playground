8c97101bab4f07376240893a77c90d4b5f6f6889
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-05-20 16:45:26 +0200

    Ping workers from master

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
+
+  // Ping workers
+  setInterval(() => {
+    for (const id in cluster.workers) {
+      cluster.workers[id].send({ cmd: 'ping' });
+    }
+  }, 2000);
+
+  // Worker event handlers
   for (const id in cluster.workers) {
     cluster.workers[id].on('message', messageHandler);
   }
@@ -23,12 +32,19 @@ if (cluster.isMaster) {
     console.log(`Worker ${worker.process.pid} died`);
   });
 } else {
+  const messageHandler = (msg) => {
+    if (msg.cmd && msg.cmd === 'ping') {
+      console.log(`Pong ${process.pid}`);
+    }
+  }
+
   http.createServer((req, res) => {
     console.log(`Worker ${process.pid} handled an HTTP request`);
     process.send({ cmd: 'notifyRequest' });
     res.writeHead(200);
     res.end('hello world\n');
   }).listen(3000);
+  process.on('message', messageHandler);
 
   console.log(`Worker ${process.pid} started`);
 }
